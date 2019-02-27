57ab54b8ea93cf4d2e088278641362edea848500
Tony Prpich <46456968+TonyPrpich@users.noreply.github.com>
2019-02-26 20:50:34 -0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,49 @@
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var randomLetter = alphabet[Math.floor(Math.random() * 26)];
+var computerChoice = randomLetter;
+var wins = 0;
+var losses = 0;
+var left = 9;
+var guesses = 9;
+var guessesSoFar = [];
+var psychicLetter;
+
+
+var newLetter = function () {
+    psychicLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+};
+
+var soFar = function () {
+    document.getElementById("guesses").innerHTML = "Guesses so far: " + guessesSoFar.join(",");
+};
+
+var guessesLeft = function () {
+    document.getElementById("left").innerHTML = "Guesses Left: " + left;
+};
+
+var newGame = function () {
+    guessedLetters = [];
+    left = 9;
+    newLetter();
+    guessesLeft();
+    soFar();
+}
+
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+    left--;
+    guessesSoFar.push(userGuess);
+    soFar();
+    guessesLeft();
+    if (left > 0) {
+        if (userGuess == psychicLetter) {
+            wins++;
+            document.getElementById("wins").innerHTML = "Wins:" + wins;
+            newGame();
+        }
+    } else if (left == 0) {
+        losses++;
+        document.getElementById("losses").innerHTML = "Losses:" + losses;
+        newGame();
+    }
+};
